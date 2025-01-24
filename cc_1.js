cb07f06ed532d4b9e753f0f72f529210bd8b0978
Gmailvicente04 <vicente.oswald@gmail.com>
2025-01-24 12:56:55 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -14,3 +14,20 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
 console.log("Is Active:", isActive, "Type:", typeof isActive);
+
+// Task 2: Primitive Data Types
+// Scenario: Product Details
+
+// Step 1: Declare a variable productName using let and assign it a string value
+let productName = "Air Jordan 1";
+
+// Step 2: Declare a variable productPrice using const and assign it a numerical value
+const productPrice = 299.99;
+
+// Step 3: Declare a variable isAvailable using var and assign it a boolean value
+var isAvailable = true;
+
+// Step 4: Log each variable's value and its type to the console
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
